f62dd3e46cfd0752ccf13371e74a89e52e448e6c
Jacobv2410 <jacobv2410@gmail.com>
2018-03-31 09:42:39 -0700

    hello

--- liri.js
+++ liri.js
@@ -1,4 +1,8 @@
 require("dotenv").config();
+var inq = require('inquirer')
+var pmpt = inq.createPromptModule()
+
+
 
 var spotify = process.env.SPOTIFY_ID
 var client = process.env.TWITTER_ACCESS_TOKEN_KEY
@@ -7,14 +11,25 @@ var omdbKey = process.env.OMDB_API_KEY
 
 // console.log(spotify, client, omdbKey)
 
-// 
+// question constructors
 
 // Variables to take in the command line information for each different API
-
-
 var action = process.argv[2]
 //should be in switch statements
 var value = process.argv[3]
+
+
+function Question (name, message) {
+    this.type = 'input'
+    this.name = name
+    this.message = message
+}
+
+pmpt([new Question('preference', 'What would you like to do? Spotify, Twitter, OMDB?')]).then(function (r) {
+    if (r.preference === 'OMDB') {
+      console.log(getMovie)  
+    }
+
 
 
 var request = require('request');
@@ -24,15 +39,18 @@ var http = require('http');
 
 // getMovie();
 
+
 function getMovie() {
+
+    var movie = "Pacific Rim"
 
     request("https://www.omdbapi.com/?apikey="+ process.env.OMDB_API_KEY +"&t=" + value, function (error, response, body) {
         if(error){
             console.log(error)
         }
 
-        // console.log(JSON.parse(body)['Title'])
-        // console.log(JSON.parse(body).Title)
+        console.log(JSON.parse(body)['Title'])
+        console.log(JSON.parse(body).Title)
         
     });
 }
@@ -57,7 +75,7 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 });
 
 
-
+})
 // function getMovies(arr) {
 //     for (let i = 0; i < arr.length; i++) {
 //         request('https://www.omdbapi.com/?apikey=60f7bdd3&t=' + movies[i],
